a606468b63c7cf9a8498721c5d789f06b706b3d0
“paul <pblack9455@yahoo.com>
2020-09-13 11:40:48 +0100

    util unit tests

---
+++ Parking.Library.Test/UtilTest.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace Parking.Library.Test
+{
+    public class UtilTest :BaseUnitTest
+    {
+        [Fact]
+        public void GivenValidDateRangeWith3Days_WhenGetDaysCalled_ThenReturns3Days()
+        {
+            // arrange
+            var fromDateTime = DateTimeParse("07/09/2017 16:50:00");
+            var toDateTime = DateTimeParse("09/09/2017 19:15:00");
+
+            // act
+            var days = Util.GetDays(fromDateTime, toDateTime);
+
+            // assert
+            Assert.Equal(3, days);
+        }
+
+        [Fact]
+        public void GivenValidDateRange_WhenCallGetDays_ThenReturnExpectedResult()
+        {
+            // arrange
+            var fromDateTime = DateTimeParse("07/09/2020 07:50:00");
+            var toDateTime = DateTimeParse("13/09/2020 05:20:00");
+
+            // act
+            var days = Util.GetDays(fromDateTime, toDateTime);
+
+            // assert
+            Assert.Equal(7, days);
+        }
+    }
+}
