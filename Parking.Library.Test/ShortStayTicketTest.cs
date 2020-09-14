ad2ba362a4fca1c67aa4ff234fa9e5ec351a25ec
“paul <pblack9455@yahoo.com>
2020-09-14 03:03:24 +0100

    fix short stay 'integration test' to unit test via Moq

--- Parking.Library.Test/ShortStayTicketTest.cs
+++ Parking.Library.Test/ShortStayTicketTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Moq;
 
 namespace Parking.Library.Test
 {
@@ -7,53 +8,76 @@ namespace Parking.Library.Test
     {
         const float hourlyRate = 1.1F;
 
+        Mock<IDurationCalculator> mockDurationCalc = null; 
+
+        public ShortStayTicketTest()
+        {
+            this.mockDurationCalc = new Mock<IDurationCalculator>();
+        }
+
         [Fact]
-        public void TestShortTicketFromSample33()
+        public void GivenValidDates1Day1Hour10Mins_When_Then()
         {
             // arrange
             var fromDateTime = DateTimeParse("07/09/2017 16:50:00");
             var toDateTime = DateTimeParse("09/09/2017 19:15:00");
-            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, new HourlyCalculator(8, 18));
+
+            this.mockDurationCalc
+                .Setup(m => m.GetTotalChargeableQuantity(It.Is<DateTime>(d => d == fromDateTime), It.Is<DateTime>(d => d == toDateTime)))
+                .Returns(11.16d);
+            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, this.mockDurationCalc.Object);
+
+            var expectedChange = 12.28F;
 
             // act
             var charge = shortTicket.CalculateCharge(toDateTime);
 
             // assert
-            Assert.Equal(Round(12.28F), Round(charge.Value));
+            Assert.Equal(Round(expectedChange), Round(charge.Value));
         }
 
         [Fact]
-        public void Test1ShortTicketFromSample33()
+        public void GivenWeekendDateStay_WhenCalculatedCalled_ThenReturnNoCharge()
         {
             // arrange
             var fromDateTime = DateTimeParse("09/09/2017 06:50:00");
             var toDateTime = DateTimeParse("09/09/2017 19:15:00");
-            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, new HourlyCalculator(8, 18));
-            
-            var expectedCharge = 0.0; //none it's a weekend
 
-            //act
+            this.mockDurationCalc
+                .Setup(m => m.GetTotalChargeableQuantity(It.Is<DateTime>(d => d == fromDateTime), It.Is<DateTime>(d => d == toDateTime)))
+                .Returns(0.0D);
+            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, this.mockDurationCalc.Object);
+            
+            var expectedCharge = 0.0; //none it's a weekend-noo charge
+
+            // act
             var charge = shortTicket.CalculateCharge(toDateTime);
 
-//
+            // assert
             Assert.Equal(Round(expectedCharge), Round(charge.Value));
+            mockDurationCalc.Verify(m => m.GetTotalChargeableQuantity(It.Is<DateTime>(d => d == fromDateTime), It.Is<DateTime>(d => d == toDateTime)));
         }
 
         [Fact]
-        public void Test2ShortTicketFromSample33()
+        public void GivenValidStayOf2Days1Hour10Mins_WhenCalculationCalled_ThenReturn23_28()
         {
             // arrange
             var fromDateTime = DateTimeParse("07/09/2020 16:50:00");
             var toDateTime = DateTimeParse("09/09/2020 19:15:00");
-            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, new HourlyCalculator(8, 18));
 
-            var expectedCharge = 21.16 * hourlyRate;  // 1hr 1m + 20hrs(2days) = 21h 10m or 11.16 decimal
+            this.mockDurationCalc
+                .Setup(m => m.GetTotalChargeableQuantity(It.Is<DateTime>(d => d == fromDateTime), It.Is<DateTime>(d => d == toDateTime)))
+                .Returns(21.16d);
+            var shortTicket = new ShortStayTicket(fromDateTime, Constants.ShortStayPrice, this.mockDurationCalc.Object);
+
+            var expectedCharge = 21.16 * hourlyRate;  // 1hr 10m + 20hrs(2days) = 21h 10m or 21.16 double
 
             // act
             var charge = shortTicket.CalculateCharge(toDateTime);
 
             // asert
             Assert.Equal(Round(expectedCharge), Round(charge.Value));
+            mockDurationCalc.Verify(m => m.GetTotalChargeableQuantity(It.Is<DateTime>(d => d == fromDateTime), It.Is<DateTime>(d => d == toDateTime)));
         }
     }
 }
